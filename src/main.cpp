a14e0896e79ba039b7e9d7330666a22bfbf7a0f5
Snipe-ctrl <joshuarodli19@gmail.com>
2025-12-15 12:00:59 -0800

    util: setup sfml

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main() {
-    cout << "Test" << endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Window");
+
+    const sf::Font font("/System/Library/Fonts/Helvetica.ttc");
+    sf::Text text(font, "Hello SFML", 50);
+    
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.draw(text);
+
+        window.display();
+    }
 }
