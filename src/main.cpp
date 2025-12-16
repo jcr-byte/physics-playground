d45d224850f8c5606322392e598d5142d1d9cac4
Snipe-ctrl <joshuarodli19@gmail.com>
2025-12-15 22:51:50 -0800

    cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,10 @@ using namespace std;
 int main() {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Window");
 
-    const sf::Font font("/System/Library/Fonts/Helvetica.ttc");
-    sf::Text text(font, "Hello SFML", 50);
-
-    sf::CircleShape circle(50.0f);
-    circle.setFillColor(sf::Color::Red);
-    circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
-
-    sf::Vector2u windowSize = window.getSize();
-    circle.setPosition(sf::Vector2f(windowSize.x / 2.0f, windowSize.y / 2.0f));
-
     EventHandler eventHandler;
 
     Mass mainMass(sf::Vector2f(0, 0), 3, 20);
 
-    
     while (window.isOpen()) {
         if (eventHandler.processEvents(window)){
             window.close();
