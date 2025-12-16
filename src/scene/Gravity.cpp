ea79dadfa31a62efa0a020928934b4dbf5203e71
Joshua Rodli <joshuarodli19@gmail.com>
2025-12-16 15:10:46 -0800

    added framerate display to scene 1

--- src/scene/Gravity.cpp
+++ src/scene/Gravity.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <SFML/Graphics.hpp>
 #include "../core/EventHandler.h"
 #include "../entities/Mass.h"
@@ -16,14 +17,43 @@ void sceneOne() {
 
     Mass mainMass(sf::Vector2f(0, 0), 3, 20);
 
+    // logic for fps text display
+    sf::Font font;
+    if (!font.openFromFile("C:/Windows/Fonts/arial.ttf")) {
+        throw std::runtime_error("Failed to load font");
+    }
+    sf::Text fpsText(font, "FPS: --");
+    fpsText.setFont(font);
+    fpsText.setCharacterSize(24);
+    fpsText.setFillColor(sf::Color::White);
+    fpsText.setPosition(sf::Vector2f(10, 10));
+
+    // logic for calculating fps
+    sf::Clock frameClock;
+    float frameTime = 0.0f;
+    float fps = 0.0f;
+    float fpsUpdateTimer = 0.0f;
+    const float FPS_UPDATE_TIME = 0.1f;
+
     while (window.isOpen()) {
         if (eventHandler.processEvents(window)){
             window.close();
         }
 
+        // logic for displaying fps
+        frameTime = frameClock.restart().asSeconds();
+        fps = 1.0f / frameTime;
+        fpsUpdateTimer += frameTime;
+
+        if (fpsUpdateTimer >= FPS_UPDATE_TIME) {
+            std::stringstream ss;
+            ss << "FPS: " << std::fixed << std::setprecision(1) << fps;
+            fpsText.setString(ss.str());
+            fpsUpdateTimer = 0.0f;
+        }
         window.clear();
 
-        mainMass.draw(window);
+        window.draw(fpsText);
         window.draw(ground);
 
         window.display();
